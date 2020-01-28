44562395fe64b8fe2f05230a07d410e372933992
EzequielMussambe <chipilicafamily@gmail.com>
2020-01-27 23:02:02 -0500

    update_app

---
+++ home.py
@@ -0,0 +1,27 @@
+from kivy.app  import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.garden.mapview import MapView
+
+class HomeScreen(Screen):
+    pass
+
+class SchoolScreen(Screen):
+    pass
+
+class HospitalScreen(Screen):
+    pass
+
+class SupermarketScreen(Screen):
+    pass
+
+main_kv=Builder.load_file("home.kv")
+class MainApp(App):
+    def build(self):
+        return main_kv
+
+
+if __name__ == "__main__":
+    MainApp().run()
